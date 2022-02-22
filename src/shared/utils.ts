925bc9ec702c8b4783cd3cb5d871e16368700050
Charlie Sparks <sparksfullstack@gmail.com>
2022-02-22 14:26:54 -0800

    eth polling working--pointed at sandbox

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -5,21 +5,17 @@ export const pollEthPrice = async () => {
     let response
     new Promise(async (resolve, reject) => {
         try {
-            response = await axios.get('https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=ETH', {
+            response = await axios.get('https://rest-sandbox.coinapi.io/v1/assets/ETH', {
                 headers: {
-                    'X-CMC_PRO_API_KEY': process.env.COIN_MARKET_CAP_API_KEY,
+                    'X-CoinAPI-KEY': process.env.COINAPI_KEY,
                 },
             });
         } catch (ex) {
             response = null;
-            // error
-            console.log(ex);
             reject(ex);
         }
         if (response) {
-            // success
             const json = response.data;
-            console.log(json.data.ETH.quote);
             resolve(json);
         }
     });
